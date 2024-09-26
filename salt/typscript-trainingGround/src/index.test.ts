3811898edb0cf3fce3905fc3f8176c0bf66254e0
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-09-26 04:50:23 +0200

    added a new test

--- salt/typscript-trainingGround/src/index.test.ts
+++ salt/typscript-trainingGround/src/index.test.ts
@@ -15,3 +15,14 @@ describe('ts tests', () => {
     assert.strictEqual(result, 'Hello Marcus, you are 51 years old');
   });
 });
+it("returns true if age is above or equal to 35", () => {
+  // act
+  const is34Old = isOld(34);
+  const is35Old = isOld(35);
+  const is36Old = isOld(36);
+
+  // assert
+  assert.strictEqual(is34Old, false);
+  assert.strictEqual(is35Old, true);
+  assert.strictEqual(is36Old, true);
+});
