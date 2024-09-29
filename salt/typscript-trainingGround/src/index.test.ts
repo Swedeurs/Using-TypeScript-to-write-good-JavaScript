07d7186f96bfe0373b3895073517536540426b13
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-09-29 15:22:27 +0200

    Removed comments

--- salt/typscript-trainingGround/src/index.test.ts
+++ salt/typscript-trainingGround/src/index.test.ts
@@ -20,7 +20,7 @@ describe("Address Tests", () => {
   });
 });
 
-// Tests for Order
+
 describe("Order Tests", () => {
   it("should correctly represent order", () => {
     const order: Order = { orderId: 12345, totalAmount: 299.99 }; // Ensure orderId is a number
@@ -29,69 +29,68 @@ describe("Order Tests", () => {
   });
 });
 
-// Tests for Generics
 describe("Generics Tests", () => {
   it("add to list of people", () => {
-    // arrange
+   
     const listOfPeople: IPerson[] = [{ name: "Marcus", birthYear: 1972 }];
 
-    // act
+
     const numberOfPeople = addToStart<IPerson>(listOfPeople, {
       name: "David",
       birthYear: 1975,
     });
 
-    // assert
+
     assert.strictEqual(numberOfPeople[0].name, "David");
     assert.strictEqual(numberOfPeople[1].name, "Marcus");
   });
 
   it("add to list of addresses", () => {
-    // arrange
+
     const listOfAddresses: Address[] = [
       { street: "Strålgatan", streetNo: 23, city: "Stockholm" },
       { street: "SchraeschazschStrasse", streetNo: 2, city: "Amsterdam" },
     ];
 
-    // act
+
     const numberOfAddresses = addToStart<Address>(listOfAddresses, {
       street: "Champs Elysee",
       streetNo: 1,
       city: "Paris",
     });
 
-    // assert
+ 
     assert.strictEqual(numberOfAddresses[0].city, "Paris");
     assert.strictEqual(numberOfAddresses[1].city, "Stockholm");
   });
 
   it("wrapper for addresses", () => {
-    // arrange
+
     const listOfAddresses: Address[] = [
       { street: "Strålgatan", streetNo: 23, city: "Stockholm" },
       { street: "SchraeschazschStrasse", streetNo: 2, city: "Amsterdam" },
       { street: "Champs Elysee", streetNo: 1, city: "Paris" },
     ];
 
-    // act
+
     const list = new Wrapper<Address>(listOfAddresses);
 
-    // assert
+
     assert.strictEqual(list.getFirst().city, "Stockholm");
     assert.strictEqual(list.getLast().city, "Paris");
   });
 
   it("wrapper for people", () => {
-    // arrange
+
     const listOfPeople: IPerson[] = [
       { name: "Marcus", birthYear: 1972 },
       { name: "David", birthYear: 1975 },
     ];
 
-    // act
+
     const peopleList = new Wrapper<IPerson>(listOfPeople);
 
-    // assert
+
     assert.strictEqual(peopleList.getFirst().name, "Marcus");
     assert.strictEqual(peopleList.getLast().name, "David");
   });
