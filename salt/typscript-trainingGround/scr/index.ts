b69ab11a0e7d14265778f364dc90f45df9ae7979
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-09-29 17:24:55 +0200

    Delted none nessecary files

--- salt/typscript-trainingGround/scr/index.ts
+++
@@ -1,6 +0,0 @@
-function greet(name: string, birthYear: number): string {
-    const age = new Date().getFullYear() - birthYear; // Calculate age based on the current year and birth year
-    return `Hello ${name}, you are ${age} years old`; // Return greeting message with name and age
-  }
-  
-  export { greet }; // Export the greet function for use in tests and other modules
